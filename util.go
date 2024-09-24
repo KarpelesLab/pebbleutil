a0bf606bbc6c889ddd34f3882d5eb7caf7caefcb
Mark Karpeles <magicaltux@gmail.com>
2024-09-24 18:49:11 +0900

    add pebbleutil.GetDup method

--- util.go
+++ util.go
@@ -1,5 +1,19 @@
 package pebbleutil
 
+import (
+	"io"
+	"slices"
+)
+
+// GetDup allows duplicating the result of a get
+func GetDup(b []byte, cl io.Closer, err error) ([]byte, error) {
+	if err != nil {
+		return nil, err
+	}
+	defer cl.Close()
+	return slices.Clone(b), nil
+}
+
 // must panics if err is not nil
 func must[T any](v T, err error) T {
 	if err != nil {
